1e580ed7f898a58853a61127a16328330a156022
Martin Lenart <martin_lenart@icloud.com>
2021-09-28 15:32:00 +0200

    Exercise Update

--- FriendWithProperties/Program.cs
+++ FriendWithProperties/Program.cs
@@ -23,6 +23,38 @@ namespace FriendWithProperties
         public string PhoneNr { get; set; }
         public FriendLevel Level { get; init; }
         public DateTime BirthDay { get; init; }
+
+        public Friend() { }
+        public Friend(FriendLevel friendLevel)
+        {
+            switch (friendLevel)
+            {
+                case FriendLevel.Friend:
+                    Name = "John";
+                    PhoneNr = "(123) 12345";
+                    Level = friendLevel;
+                    BirthDay = DateTime.Parse("1990-01-01");
+                    break;               
+                case FriendLevel.ClassMate:
+                    Name = "Peter";
+                    PhoneNr = "(123) 12345";
+                    Level = friendLevel;
+                    BirthDay = DateTime.Parse("1995-01-01");
+                    break;
+                case FriendLevel.Family:
+                    Name = "Fido the puppy";
+                    PhoneNr = "(123) 12345";
+                    Level = friendLevel;
+                    BirthDay = DateTime.Parse("2020-01-01");
+                    break;
+                case FriendLevel.BestFriend:
+                    Name = "Alice";
+                    PhoneNr = "(123) 12345";
+                    Level = friendLevel;
+                    BirthDay = DateTime.Parse("2020-01-01");
+                    break;
+            }
+        }
     }
     class Program
     {
@@ -46,8 +78,11 @@ namespace FriendWithProperties
             Friend myBestFriend = new Friend(FriendLevel.BestFriend);
             Console.WriteLine(myBestFriend.Name);
 
-            Friend aFamilyMember = new Friend(FriendLevel.Family);
-            Console.WriteLine(aFamilyMember.Name);
+            Friend aFamilyMember1 = new Friend(FriendLevel.Family);
+            Console.WriteLine(aFamilyMember1.Name);
+
+            Friend aFamilyMember2 = new Friend(FriendLevel.Family);
+            Console.WriteLine(aFamilyMember1 == aFamilyMember2);
         }
     }
 }
@@ -55,3 +90,4 @@ namespace FriendWithProperties
 //1.    Create a constructor that takes one parameter, Level, of type FriendLevel. In the Constructor
 //      use a switch-case construct to initialize  Name, PhoneNr, Level and BirthDay of a person you know
 //      of that particular FriendLevel
+//2.    Overload operators == and != to decide if two objects of class Friends are equal.
