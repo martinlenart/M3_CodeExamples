efa3237e5a79e7f487b60ec01089ab0f0a73786a
Martin Lenart <martin_lenart@icloud.com>
2021-09-28 11:34:53 +0200

    Exercise Update

--- DeckOfCards/Program.cs
+++ DeckOfCards/Program.cs
@@ -27,7 +27,14 @@ namespace DeckOfCards
 			}
 		}
 		public string StringToPrint => $"{Value} of {Color}, a {BlackOrRed} card";
-
+		//exactly the same as above
+		public string StringToPrint1
+		{
+			get
+			{
+				return $"{Value} of {Color}, a {BlackOrRed} card";
+			}
+		}
 	}
 	class Program
     {
@@ -38,8 +45,15 @@ namespace DeckOfCards
         }
     }
 }
-//Exercises
+//Group Exercise1
 //1.	Modify PlayingCard so the Data Members Color and Value becomes properties that can be read by all users of the type but only set during initialization
 //2.	Modify PrintOut() so it becomes a property named StringToPrint
 //3.	Add a property of type string to PlayingCard named BlackOrRed that returns the string "Black" for Clubs and Spades, otherwise "Red"
 //4.	Modify StringToPrint to also include if the card is Red or Black in the string to printout
+
+//Group Exercise2
+//1.	Create a constructor to PlayingCard that takes one parameter, HigestOrLowest, of type bool
+//		and if true initialize the instance to Ace of Spades, otherwise initializes the instance to Two of Clubs.
+//2.	Create another constructor that takes two parameters HigestOrLowest (type bool) and Color (type PlayingCardColor).
+//		Creates an instance of the higest and lowest card of that specific color
+
