7856c4b9b9ce72f9857bfcf15718c2838e32c342
Martin Lenart <martin_lenart@icloud.com>
2021-09-30 14:53:19 +0200

    MyCars added

---
+++ MyCar/Car.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCar
+{
+    public enum CarColor
+    {
+        Brown, Red, Green, Burgundy
+    }
+    public enum CarBrand
+    {
+        Boxcar, Ford, Jaguar, Honda
+    }
+    public enum CarModel
+    {
+        Boxmodel, Mustang_GT, XF, Civic
+    }
+    class Car
+    {
+        public CarColor Color { get; set; }
+        public CarBrand Brand { get; set; }
+        public CarModel Model { get; set; }
+
+        public int KW { get; set; } = 2000;
+        public int Year { get; }
+
+        public override string ToString()
+        {
+            return $"Brand: {Brand}, Model: {Model}, Color: {Color}, KW: {KW}, Year: {Year}";
+        }
+
+        public Car ()
+        {
+            Color = CarColor.Burgundy;
+            Brand = CarBrand.Honda;
+            Model = CarModel.Civic;
+            Year = 2021;
+        }
+        public Car (bool Favorite) : this()
+        {
+            if (Favorite)
+            {
+                Color = CarColor.Red;
+                Brand = CarBrand.Ford;
+                Model = CarModel.Mustang_GT;
+                Year = 1978;
+                KW = 4000;
+            }
+        }
+    }
+}
---
+++ MyCar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace MyCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car myCar1 = new Car { };
+            Console.WriteLine(myCar1);
+    
+            Car myCar2 = new Car ();
+            Console.WriteLine(myCar2);
+
+            Car myCar3 = new Car { Color = CarColor.Green };
+            Console.WriteLine(myCar3);
+
+            Car myCar4 = new Car(true);
+            Console.WriteLine(myCar4);
+  
+            Car myCar5 = new Car(false);
+            Console.WriteLine(myCar5);
+        }
+    }
+}
