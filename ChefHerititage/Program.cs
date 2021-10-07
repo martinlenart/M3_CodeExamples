7cab2629f1c832d12cbc5f21cf6a4e359965e510
Martin Lenart <martin_lenart@icloud.com>
2021-10-07 12:06:46 +0200

    Inheritance Update

--- ChefHerititage/Program.cs
+++ ChefHerititage/Program.cs
@@ -7,55 +7,72 @@ namespace ChefHerititage
         public string Name { get; set; }
         public int Age = 0;
         public virtual string Hello => "I'm busy!";
+
+        public string FavoriteDish => "I have none";
     }
     public class FrenchChef : Chef
     {
-        public new int Age = 30;
+        public new int Age;
         public override string Hello => "Bonjour";
         public override string ToString() => $"{Hello} my name is {Name}";
 
+        public int Salary;
+
         public FrenchChef()
         {
-            base.Age = 60;
+            base.Age = 35;
+            this.Age = 20;
         }
     }
     public class ItalianChef : Chef
     {
-        public new string Hello => "Ciao";
+        public override string Hello => "Ciao";
         public override string ToString() => $"{Hello} my name is {Name}";
     }
     public class SwedishChef : Chef
     {
-        public new string Hello => "Hej";
+        public override string Hello => "Hej";
         public override string ToString() => $"{Hello} my name is {Name}";
     }
     class Program
     {
         static void Main(string[] args)
         {
-            FrenchChef chef1 = new FrenchChef { Name = "Jean-Pierre" };
-            ItalianChef chef2 = new ItalianChef { Name = "Mario" };
-            SwedishChef chef3 = new SwedishChef { Name = "Pelle" };
+            FrenchChef french = new FrenchChef { Name = "Jean-Pierre" };
+            ItalianChef italian = new ItalianChef { Name = "Mario" };
+            SwedishChef swedish = new SwedishChef { Name = "Pelle" };
 
-            Console.WriteLine(chef1.Hello);
-            Chef myBoss = (Chef)chef1;
-            Console.WriteLine(myBoss.Hello);
+            Console.WriteLine(french.Hello);
+            Console.WriteLine(italian.Hello);
+            Console.WriteLine(swedish.Hello);
 
-            Console.WriteLine("\n\n\n");
-            Console.WriteLine(chef1);
-            Console.WriteLine(chef1.Age);
-
-            Chef boss = chef1;
-            Console.WriteLine(boss.Age);
-
-            Console.WriteLine(chef2);
-            Console.WriteLine(chef2.Age);
-            Console.WriteLine(chef3);
-
-            Chef masterChef = new Chef { Name = "Gordon" };
             Console.WriteLine();
-            Console.WriteLine(masterChef.Name);
+
+            Chef anyChef = new Chef { };
+            Chef chef1 = new FrenchChef { Name = "Jean-Pierre" };
+            FrenchChef french3 = new FrenchChef { Name = "Jean-Pierre" };
+            
+            SayHello(chef1);
+            MyFavoriteDish(chef1);
+
+            //FrenchChef french3 = (FrenchChef) new Chef { };     // Error
+            //Console.WriteLine(french3.Hello);
 
         }
+
+        public static void MyFavoriteDish(Chef myChef)
+        {
+            Console.WriteLine(myChef.Hello);
+        }
+
+        public static void SayHello (Chef myChef)
+        {
+            Console.WriteLine(myChef.Hello);
+        }
+        public static void SayHello1(FrenchChef myChef)
+        {
+            Console.WriteLine(myChef.Hello);
+        }
+
     }
 }
