58b6d1f63e9af68b1a72a2f0930c0cca47ee4e7f
Martin Lenart <martin_lenart@icloud.com>
2021-10-13 10:47:37 +0200

    UserInputPractise updated with answers

--- UserInputPractise/Program.cs
+++ UserInputPractise/Program.cs
@@ -28,6 +28,33 @@ namespace UserInputPractise
 
             Console.WriteLine($"\nYou have {NrSiblings} brothers and sisters and {NrCousins} cousins");
 
+            #region Exercise answers
+
+            Console.WriteLine("\n\nUsing Exercise Answers");
+            Continue =  UserInput.TryReadString("What is your name", out YourName) &&
+                        UserInput.TryReadString("What is your father's name", out FathersName) &&
+                        UserInput.TryReadString("What is your mother's name", out MothersName);
+                        // Terminate if Quit
+            if (!Continue) return;
+
+            Console.WriteLine($"\nHi {YourName}. Your father's name is {FathersName} and your mother's name is {MothersName}");
+
+            Continue = UserInput.TryReadInt32("How many brothers and sisters do you have", 0, 20, out NrSiblings) &&
+                       UserInput.TryReadInt32("How many cousins do you have", 0, 200, out NrCousins);
+
+            // Terminate if Quit
+            if (!Continue) return;
+
+            Console.WriteLine($"\nYou have {NrSiblings} brothers and sisters and {NrCousins} cousins");
+
+            DateTime BirthDay = default;
+            Continue = UserInput.TryReadDateTime("What is your birthday", out BirthDay);
+            
+            // Terminate if Quit
+            if (!Continue) return;
+
+            Console.WriteLine($"\nYour birthday is {BirthDay:d}");
+            #endregion
         }
 
         #region Try pattern for User Console input
